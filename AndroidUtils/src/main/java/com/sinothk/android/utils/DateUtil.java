fe50b016e2b5fe40a810c2ccb04a1ff5a8b99c35
LiangYT <381518188@qq.com>
2019-11-05 18:04:36 +0800

    完善功能

--- AndroidUtils/src/main/java/com/sinothk/android/utils/DateUtil.java
+++ AndroidUtils/src/main/java/com/sinothk/android/utils/DateUtil.java
@@ -133,7 +133,10 @@ public class DateUtil {
         }
 
         long day = (new Date().getTime() - birthday.getTime()) / (24 * 60 * 60 * 1000) + 1;
-        return (int) (day / 365);
+
+        int age = (int) (day / 365);
+
+        return age == 0 ? 1 : age;
     }
 
     /**
@@ -353,7 +356,7 @@ public class DateUtil {
 
         if (dateTime < ys) {
             // 不是同一年
-            return new SimpleDateFormat("MM月dd日", Locale.getDefault()).format(date);
+            return new SimpleDateFormat("yyyy年MM月", Locale.getDefault()).format(date);
         } else {
             if ((ds - yd) == (48 * 60 * 60 * 1000)) {
                 return new SimpleDateFormat("前天 HH:mm", Locale.getDefault()).format(date);
@@ -471,4 +474,37 @@ public class DateUtil {
 
         return "星 期 " + Week;
     }
+
+    /**
+     * Java通过生日计算星座
+     *
+     * @param month
+     * @param day
+     * @return
+     */
+    public String getConstellation(int month, int day) {
+        return day < dayArr[month - 1] ? constellationArr[month - 1] : constellationArr[month];
+    }
+
+    /**
+     * 通过生日计算属相
+     *
+     * @param year
+     * @return
+     */
+    public String getYear(int year) {
+        if (year < 1900) {
+            return "未知";
+        }
+        int start = 1900;
+        String[] years = new String[]{"鼠", "牛", "虎", "兔", "龙", "蛇", "马", "羊",
+                "猴", "鸡", "狗", "猪"};
+        return years[(year - start) % years.length];
+    }
+
+    private final static String[] constellationArr = new String[]{"摩羯座",
+            "水瓶座", "双鱼座", "白羊座", "金牛座", "双子座", "巨蟹座", "狮子座", "处女座", "天秤座",
+            "天蝎座", "射手座", "摩羯座"};
+
+    private final static int[] dayArr = new int[]{20, 19, 21, 20, 21, 22, 23, 23, 23, 24, 23, 22};
 }
